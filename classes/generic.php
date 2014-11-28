84191dd092e3289017aec7c17eabe00c454f2874
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-11-28 13:02:44 +0100

    Finally, trait is usefull ;).

--- classes/generic.php
+++ classes/generic.php
@@ -4,39 +4,7 @@ namespace estvoyage\value;
 
 abstract class generic
 {
-	private
-		$values
-	;
-
-	function __get($property)
-	{
-		if (! isset($this->{$property}))
-		{
-			throw new \logicException('Undefined property: ' . get_class($this) . '::' . $property);
-		}
-
-		return $this->values[$property];
-	}
-
-	function __set($property, $value)
-	{
-		throw new \logicException(get_class($this) . ' is immutable');
-	}
-
-	function __unset($property)
-	{
-		throw new \logicException(get_class($this) . ' is immutable');
-	}
-
-	function __isset($property)
-	{
-		/*
-		isset() is very fast but can fail if $this->values[$property] === null
-		so using array_key_exists as fallback to be sure that the key does not exist
-		is a good trade-off between reliability and performance
-		*/
-		return isset($this->values[$property]) || array_key_exists($property, $this->values);
-	}
+	use immutable;
 
 	protected function __construct(array $values)
 	{
---
+++ classes/immutable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace estvoyage\value;
+
+trait immutable
+{
+	private
+		$values
+	;
+
+	function __get($property)
+	{
+		if (! isset($this->{$property}))
+		{
+			throw new \logicException('Undefined property: ' . get_class($this) . '::' . $property);
+		}
+
+		return $this->values[$property];
+	}
+
+	function __set($property, $value)
+	{
+		throw new \logicException(get_class($this) . ' is immutable');
+	}
+
+	function __unset($property)
+	{
+		throw new \logicException(get_class($this) . ' is immutable');
+	}
+
+	function __isset($property)
+	{
+		/*
+		isset() is very fast but can fail if $this->values[$property] === null
+		so using array_key_exists as fallback to be sure that the key does not exist
+		is a good trade-off between reliability and performance
+		*/
+		return isset($this->values[$property]) || array_key_exists($property, $this->values);
+	}
+}
