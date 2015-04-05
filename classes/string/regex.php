b7a1318394bd6111d73881b7abd044089a16d9a3
Frédéric Hardy <frederic.hardy@mageekbox.net>
2015-04-05 22:01:39 +0200

    Add regex.

---
+++ classes/string/regex.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace estvoyage\value\string;
+
+use
+	estvoyage\value\string
+;
+
+final class regex extends string\notEmpty
+{
+	function __construct($value)
+	{
+		$domainException = null;
+
+		try
+		{
+			parent::__construct($value);
+		}
+		catch (\domainException $domainException) {}
+
+		if ($domainException || ! self::isValidRegex($value))
+		{
+			throw new \domainException('Regular expression should be a valid PCRE pattern');
+		}
+	}
+
+	static function validate($value)
+	{
+		return parent::validate($value) && self::isValidRegex($value);
+	}
+
+	private static function isValidRegex($value)
+	{
+		$previousErrorHandler = set_error_handler(function($errno, $errstr) {});
+
+		$isValidRegex = preg_match($value, '') !== false;
+
+		set_error_handler($previousErrorHandler);
+
+		return $isValidRegex;
+	}
+}
---
+++ tests/units/classes/string/regex.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace estvoyage\value\tests\units\string;
+
+require __DIR__ . '/../../runner.php';
+
+use
+	estvoyage\value\tests\units
+;
+
+class regex extends units\test
+{
+	function testClass()
+	{
+		$this->testedClass
+			->isFinal
+			->extends('estvoyage\value\string\notEmpty')
+		;
+	}
+
+	/**
+	 * @dataProvider invalidValueProvider
+	 */
+	function testConstructorWithInvalidValue($invalidValue)
+	{
+		$this->exception(function() use ($invalidValue) {
+				$this->newTestedInstance($invalidValue);
+			}
+		)
+			->isInstanceOf('domainException')
+			->hasMessage('Regular expression should be a valid PCRE pattern')
+		;
+	}
+
+	/**
+	 * @dataProvider invalidValueProvider
+	 */
+	function testValidateWithInvalidValue($invalidValue)
+	{
+		$this->boolean(\estvoyage\value\string\regex::validate($invalidValue))->isFalse;
+	}
+
+	protected function invalidValueProvider()
+	{
+		return [
+			'',
+			'@',
+			uniqid()
+		];
+	}
+}
